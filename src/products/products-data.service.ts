eea53287d39b4d1ecc2eacc1f0b5b72a5a8a0aff
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-24 18:19:42 +0200

    Add products data service

---
+++ src/products/products-data.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDTO } from './dto/create-product.dto';
+import { UpdateProductDTO } from './dto/update-product.dto';
+import { Product } from './interfaces/product.interface';
+import { v4 as uuidv4 } from 'uuid';
+import { dateToArray } from 'src/shared/helpers/date.helper';
+import { ExternalProductDTO } from './dto/external-product.dto';
+
+@Injectable()
+export class ProductsDataService {
+  private products: Array<Product> = [];
+
+  addProduct(_item_: CreateProductDTO): ExternalProductDTO {
+    const product: Product = {
+      ..._item_,
+      id: uuidv4(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.products.push(product);
+    return {
+      ...product,
+      createdAt: dateToArray(product.createdAt),
+      updatedAt: dateToArray(product.updatedAt),
+    };
+  }
+
+  deleteProduct(id: string): void {
+    this.products = this.products.filter((i) => i.id !== id);
+  }
+
+  updateProduct(id: string, dto: UpdateProductDTO): Product {
+    this.products.map((i) => {
+      if (i.id === id) {
+        return {
+          ...dto,
+          id: i.id,
+          createdAt: i.createdAt,
+          updatedAt: new Date(),
+        };
+      }
+      return i;
+    });
+
+    return this.getProductById(id);
+  }
+
+  getProductById(id: string): Product {
+    return this.products.find((i) => i.id === id);
+  }
+
+  getAllProducts(): Array<Product> {
+    return this.products;
+  }
+}
