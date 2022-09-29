5961c9466e854d87f255b81a1e7f04d6ac8fa219
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-29 14:41:56 +0200

    Update ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,7 @@ export = {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: '****',
+  password: 'vjEK69w8AU@K',
   database: 'shop',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
