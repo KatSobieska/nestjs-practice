5b8ec4c4ce0f47d6e7cdd673bbc7b0d513fa72fd
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-01 22:31:27 +0200

    Update ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,4 +7,11 @@ export = {
   database: 'shop',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
+  dropSchema: true,
+  migrationsRun: true,
+  migrations: [__dirname + '/db/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+  subscribers: [__dirname + '/db/subscribers/**/*{.ts,.js}'],
 };
