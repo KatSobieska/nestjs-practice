1ecb40bd9c00a345dfe8786f3ed97ce99b896596
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-03 17:21:55 +0200

    Remove useless imports

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,8 +1,4 @@
 import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
-import { UserAddress } from 'src/users/db/userAddress.entity';
-import { User } from 'src/users/db/users.entity';
-import { CreateDateColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { OrderedProducts } from '../db/orderedProducts.entity';
 
 export class CreateOrderDTO {
   @IsNotEmpty()
--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,9 +1,5 @@
 import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
-import { Product } from 'src/products/db/products.entity';
 import { Orders } from 'src/shared/enums/orders.enum';
-import { UserAddress } from 'src/users/db/userAddress.entity';
-import { User } from 'src/users/db/users.entity';
-import { OrderedProducts } from '../db/orderedProducts.entity';
 
 export class UpdateOrderDTO {
   @IsNotEmpty()
