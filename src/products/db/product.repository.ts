8db420d111b1d429dcfe856443d6a6f5f69d4a6d
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-10-04 19:24:50 +0200

    Add buildPredicate to product and user repository

--- src/products/db/product.repository.ts
+++ src/products/db/product.repository.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Repository, In, DataSource } from 'typeorm';
+import { TextFilterType } from 'src/shared/enums/textFilterType.enum';
+import {
+  Repository,
+  In,
+  DataSource,
+  FindManyOptions,
+  FindOptionsWhere,
+  Between,
+  MoreThan,
+  LessThan,
+  Like,
+  Equal,
+} from 'typeorm';
+import { ProductsQuery } from '../queries/ProductsQuery.interface';
 import { Product } from './products.entity';
 
 @Injectable()
@@ -7,11 +20,42 @@ export class ProductRepository extends Repository<Product> {
   constructor(private dataSource: DataSource) {
     super(Product, dataSource.createEntityManager());
   }
-  findProductsByName(names: string[]): Promise<Product[]> {
-    return this.find({
-      where: {
-        name: In(names),
-      },
-    });
+  private buildPredicate(query: ProductsQuery): FindManyOptions<Product> {
+    const predicate: FindOptionsWhere<Product> = {};
+
+    if (query.maxPrice && query.minPrice) {
+      predicate.price = Between(query.minPrice, query.maxPrice);
+    } else if (query.minPrice) {
+      predicate.price = MoreThan(query.minPrice);
+    } else if (query.maxPrice) {
+      predicate.price = LessThan(query.maxPrice);
+    }
+
+    if (query.name && query.nameFilterType === TextFilterType.CONTAINS) {
+      predicate.name = Like(`%${query.name}%`);
+    } else if (query.name) {
+      predicate.name = Equal(query.name);
+    }
+
+    if (query.minCount && query.maxCount) {
+      predicate.count = Between(query.minCount, query.maxCount);
+    } else if (query.minCount) {
+      predicate.count = MoreThan(query.minCount);
+    } else if (query.maxCount) {
+      predicate.count = LessThan(query.maxCount);
+    }
+
+    const findManyOptions: FindManyOptions<Product> = {
+      where: predicate,
+    };
+
+    findManyOptions.order = {
+      [query.sortField || 'createdAt']: query.orderDirection || 'ASC',
+    };
+
+    return findManyOptions;
+  }
+  findAll(_query_: ProductsQuery): Promise<Product[]> {
+    return this.find(this.buildPredicate(_query_));
   }
 }
--- src/users/db/user.repository.ts
+++ src/users/db/user.repository.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Repository, In, DataSource } from 'typeorm';
+import { TextFilterType } from 'src/shared/enums/textFilterType.enum';
+import {
+  Repository,
+  In,
+  DataSource,
+  FindManyOptions,
+  FindOptionsWhere,
+  Between,
+  MoreThan,
+  LessThan,
+  Like,
+  Equal,
+} from 'typeorm';
+import { UsersQuery } from '../queries/UsersQuery.interface';
 import { User } from './users.entity';
 
 @Injectable()
@@ -15,11 +28,58 @@ export class UserRepository extends Repository<User> {
     });
   }
 
-  findUserByEmail(emails: string[]): Promise<User[]> {
-    return this.find({
-      where: {
-        email: In(emails),
-      },
-    });
+  private buildPredicate(query: UsersQuery): FindManyOptions<User> {
+    const predicate: FindOptionsWhere<User> = {};
+
+    if (query.dateOfBirthLaterThan && query.dateOfBirthEarlierThan) {
+      predicate.dateOfBirth = Between(
+        new Date(query.dateOfBirthLaterThan),
+        new Date(query.dateOfBirthEarlierThan),
+      );
+    } else if (query.dateOfBirthLaterThan) {
+      predicate.dateOfBirth = MoreThan(new Date(query.dateOfBirthLaterThan));
+    } else if (query.dateOfBirthEarlierThan) {
+      predicate.dateOfBirth = LessThan(new Date(query.dateOfBirthEarlierThan));
+    }
+
+    if (query.firstName && query.nameFilterType === TextFilterType.CONTAINS) {
+      predicate.firstName = Like(`%${query.firstName}%`);
+    } else if (query.firstName) {
+      predicate.firstName = Equal(query.firstName);
+    }
+
+    if (query.lastName && query.nameFilterType === TextFilterType.CONTAINS) {
+      predicate.lastName = Like(`%${query.lastName}%`);
+    } else if (query.lastName) {
+      predicate.lastName = Equal(query.lastName);
+    }
+
+    if (query.email && query.nameFilterType === TextFilterType.CONTAINS) {
+      predicate.email = Like(`%${query.email}%`);
+    } else if (query.email) {
+      predicate.email = Equal(query.email);
+    }
+
+    if (query.role) {
+      predicate.role = Equal(query.role);
+    }
+
+    const findManyOptions: FindManyOptions<User> = {
+      where: predicate,
+    };
+
+    findManyOptions.order = {
+      [query.sortField || 'createdAt']: query.firstName,
+    };
+
+    return findManyOptions;
+  }
+
+  findAll(_query_: UsersQuery): Promise<User[]> {
+    return this.find(this.buildPredicate(_query_));
+  }
+
+  findUserByEmail(_query_: UsersQuery): Promise<User> {
+    return this.findOne(this.buildPredicate(_query_));
   }
 }
